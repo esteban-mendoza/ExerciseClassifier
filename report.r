d41411381d516b642c824fea33b6e87caaf55848
Esteban Mendoza <esteban.mendoza@outlook.com>
2018-08-04 20:09:52 -0500

    New version of report.r

--- report.r
+++ report.r
@@ -13,19 +13,36 @@ download.file(url_training, destfile = "data/pml-training.csv")
 download.file(url_testing, destfile = "data/pml-testing.csv")
 
 # Loading data
-training = read.csv("data/pml-training.csv", na.strings = "#DIV/0!")
+training = read.csv("data/pml-training.csv", na.strings = c("#DIV/0!","NA"))
 testing = read_csv("data/pml-testing.csv", na="#DIV/0!")
 
-# Dropping X1 variable
+# Dropping row names variable (X)
 training = as_tibble(training)
 training = select(training, -X)
 
 # Copy of training
 x = training
 
+
+## Preprocessing
+
+nvars = dim(x)[2]
+vars_to_keep = vector(length = nvars)
+
+# If a given variable has less than 90% of NA's, we keep it
+for (i in 1:nvars) {
+    if (mean(is.na(x[,i])) < 0.9) {
+        vars_to_keep[i] = TRUE
+    }
+}
+
+# Dropping variables
+x = x[,vars_to_keep]
+
+
 # Near zero variance
-nzv = nearZeroVar(training, saveMetrics = TRUE)
+nzv = nearZeroVar(x, saveMetrics = TRUE)
 x = x[,!nzv$nzv]
 
 
-fit = train(classe ~ ., data = training, method = "rpart")
+#
